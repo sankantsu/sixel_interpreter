fadbe8bfe31c1fe8f645de81f1da86d4e0c8d31b
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-06-29 19:11:47 +0900

    feat: save visualization result in file

--- sixel_interpreter/vis.py
+++ sixel_interpreter/vis.py
@@ -6,14 +6,16 @@ import matplotlib.animation as animation
 from sixel_interpreter import execute, get_frames
 
 
-def show_image(data: str) -> None:
+def show_image(data: str, *, out: str | None) -> None:
     _, ax = plt.subplots()
     img = execute(data)
     ax.imshow(img)
+    if out:
+        plt.savefig(out)
     plt.show()
 
 
-def show_animation(data: str, **kwargs: Any) -> None:
+def show_animation(data: str, *, out: str | None, **kwargs: Any) -> None:
     fig, ax = plt.subplots()
     frames = get_frames(data)
     ims = []
@@ -22,7 +24,9 @@ def show_animation(data: str, **kwargs: Any) -> None:
         if i == 0:
             ax.imshow(frame)
         ims.append([im])
-    _ = animation.ArtistAnimation(fig, ims, blit=True, repeat=False, **kwargs)
+    ani = animation.ArtistAnimation(fig, ims, blit=True, repeat=False, **kwargs)
+    if out:
+        ani.save(out)
     plt.show()
 
 
@@ -31,6 +35,7 @@ def main() -> None:
     parser.add_argument("file", nargs="?", help="Sixel file to show (default: stdin)")
     parser.add_argument("--animation", action="store_true", help="Show animation")
     parser.add_argument("--interval", type=int, default=100, help="Animation interval")
+    parser.add_argument("--out", help="Filename to save image/animation")
     args = parser.parse_args()
     if args.file:
         with open(args.file) as f:
@@ -38,9 +43,9 @@ def main() -> None:
     else:
         data = sys.stdin.read()
     if args.animation:
-        show_animation(data, interval=args.interval)
+        show_animation(data, out=args.out, interval=args.interval)
     else:
-        show_image(data)
+        show_image(data, out=args.out)
 
 
 if __name__ == "__main__":
